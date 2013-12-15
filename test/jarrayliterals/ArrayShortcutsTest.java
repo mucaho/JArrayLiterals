37007e2b5fb5b1f7e1f94f1df8c5d48f5e93e41d
mucaho <mkucko@gmail.com>
2013-12-15 23:46:26 +0100

    more strict tests

--- test/jarrayliterals/ArrayShortcutsTest.java
+++ test/jarrayliterals/ArrayShortcutsTest.java
@@ -84,32 +84,69 @@ public class ArrayShortcutsTest {
 		return out;
 	}
 
-	
-	private static final void testClass(Object object, Object expectedClass) {
-		int arrLength = 0;
-		if (expectedClass.getClass().isArray())
-			arrLength = Array.getLength(expectedClass);
+
+	private static final void testEmptyArray(Object object, Class<?> arrayClass) {
+		testClass(object, arrayClass);
 		
-		if (arrLength != 0) {
-			assertEquals("Element count mismatch.", arrLength, Array.getLength(object));
-			for (int i=0; i<arrLength; i++)
-				testClass(Array.get(object, i), Array.get(expectedClass, i));
+		int objLength = Array.getLength(object);
+		
+		Class<?> componentType = arrayClass.getComponentType();
+		if (!componentType.isArray()) {
+			assertEquals("Array should have zero elements.", 0, objLength);
 		} else {
-			assertEquals("Class mismatch.", 
-					expectedClass, object!=null ? object.getClass() : Void.class);
+			assertEquals("Array should have one element", 1, objLength);
+			
+			testEmptyArray(Array.get(object, 0), componentType);
 		}
 	}
+	
+	private static final void testClass(Object object, Class<?> expectedClass) {
+		assertEquals("Class mismatch.", 
+				expectedClass, object!=null ? object.getClass() : Void.class);
+	}
+	
+	private static final void testClasses(Object object, Object expectedClasses) {
+		int objLength = 0;
+		if (object!=null && object.getClass().isArray()) {
+			objLength = Array.getLength(object);
+		}
+		int expectedLength = 0;
+		if (expectedClasses.getClass().isArray()) {
+			expectedLength = Array.getLength(expectedClasses);
+		}
+		
+		
+		if (expectedLength != 0) { //expectedLength!=0
+			
+			assertEquals("Element count mismatch.", expectedLength, objLength);
+			for (int i=0; i<expectedLength; i++)
+				testClasses(Array.get(object, i), Array.get(expectedClasses, i));
+		
+		} else if (objLength != 0) { // expectedLength==0, objLength!=0
+			
+			testEmptyArray(object, (Class<?>) expectedClasses);
+			
+		} else { // expectedLength==0, objLength==0
+			
+			testClass(object, (Class<?>) expectedClasses);
+		}
+	}
+	
+	private static final void testName(Object test, String expectedName, Object expectedClass) {
+		if (test == null) {
+			assertEquals("Is not null!", expectedName, "null");
+			assertEquals("Is not Void!", expectedClass, Void.class);
+		} else {
+			assertEquals("Class name mismatch.", expectedName, test.getClass().getSimpleName());
+		}
+	}
+
+
 	@Test
 	@Parameters
 	public final void testShortcuts(Object test, String expectedName, Object expectedClasses) {
-		if (test == null) {
-			assertEquals("Is not null!", expectedName, "null");
-			assertEquals("Is not null!", expectedClasses, Void.class);
-			return;
-		}
-		
-		assertEquals("Class name mismatch.", expectedName, test.getClass().getSimpleName());
-		testClass(test, expectedClasses);
+		testName(test, expectedName, expectedClasses);
+		testClasses(test, expectedClasses);
 	}
 
 }
